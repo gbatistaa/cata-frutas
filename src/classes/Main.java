a12cf624d78b221a3f21dbbfb12eb6a45a256a14
derickdtc <120052482+derickdtc@users.noreply.github.com>
2024-09-27 08:46:19 -0300

    Update Main.java

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -1,10 +1,18 @@
 package classes;
-
+import javax.swing.*;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Floresta flo = new Floresta(6);
+		
+		 SwingUtilities.invokeLater(() -> {
+			 Jogo jogo = new Jogo();	
+			jogo.render(flo);
 
+			 jogo.setVisible(true);
+	        });
+	
 	}
 
 }
