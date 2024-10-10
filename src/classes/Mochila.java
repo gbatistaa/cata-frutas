88feeca132543e8e0c6edfde51ab9f73b9037356
Gabriel Batista <127104230+gbatistaa@users.noreply.github.com>
2024-10-09 23:06:45 -0300

    javadoc

--- src/classes/Mochila.java
+++ src/classes/Mochila.java
@@ -1,34 +1,63 @@
+/**
+ * A classe Mochila representa uma mochila que pode armazenar frutas.
+ */
 package classes;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Mochila {
-  private final int capacidade;
-  private final List<Fruta> frutas;
+    private final int capacidade;
+    private final List<Fruta> frutas;
 
-  public Mochila(int capacidade) {
-    this.capacidade = capacidade;
-    this.frutas = new ArrayList<>();
-  }
-
-  public boolean adicionarFruta(Fruta fruta) {
-    if (frutas.size() < capacidade) {
-      frutas.add(fruta);
-      return true;
+    /**
+     * Construtor da mochila.
+     *
+     * @param capacidade A capacidade máxima da mochila.
+     */
+    public Mochila(int capacidade) {
+        this.capacidade = capacidade;
+        this.frutas = new ArrayList<>();
     }
-    return false;
-  }
 
-  public void removerFruta(Fruta fruta) {
-    frutas.remove(fruta);
-  }
+    /**
+     * Adiciona uma fruta à mochila.
+     *
+     * @param fruta A fruta a ser adicionada.
+     * @return true se a fruta foi adicionada com sucesso, false se a mochila estiver cheia.
+     */
+    public boolean adicionarFruta(Fruta fruta) {
+        if (frutas.size() < capacidade) {
+            frutas.add(fruta);
+            return true;
+        }
+        return false;
+    }
 
-  public int getCapacidade() {
-    return capacidade;
-  }
+    /**
+     * Remove uma fruta da mochila.
+     *
+     * @param fruta A fruta a ser removida.
+     */
+    public void removerFruta(Fruta fruta) {
+        frutas.remove(fruta);
+    }
 
-  public List<Fruta> getFrutas() {
-    return frutas;
-  }
+    /**
+     * Retorna a capacidade da mochila.
+     *
+     * @return A capacidade da mochila.
+     */
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    /**
+     * Retorna a lista de frutas na mochila.
+     *
+     * @return Uma lista de frutas.
+     */
+    public List<Fruta> getFrutas() {
+        return frutas;
+    }
 }
