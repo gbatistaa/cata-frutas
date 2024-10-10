45363acf07a4fb409143da1ab9ebfdcd4bab276c
Gabriel Batista <127104230+gbatistaa@users.noreply.github.com>
2024-10-09 23:11:33 -0300

    javadoc

--- src/classes/Fruta.java
+++ src/classes/Fruta.java
@@ -1,26 +1,34 @@
+/**
+ * A classe Fruta é uma classe abstrata que representa frutas que podem ser coletadas no jogo.
+ */
 package classes;
 
 public abstract class Fruta extends Entidade {
-  private int peso = 1;
-  private boolean bichada = false;
+    private int peso = 1;
+    private boolean bichada = false;
 
-  public int getPeso() {
-    return peso;
-  }
+    public int getPeso() {
+        return peso;
+    }
 
-  public void setPeso(int peso) {
-    this.peso = peso;
-  }
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
 
-  public boolean isBichada() {
-    return bichada;
-  }
+    public boolean isBichada() {
+        return bichada;
+    }
 
-  public void setBichada(boolean bichada) {
-    this.bichada = bichada;
-  }
+    public void setBichada(boolean bichada) {
+        this.bichada = bichada;
+    }
 
-  public void aoColetar(Jogador coletor) {
-    // Logica ao coletar fruta
-  }
+    /**
+     * Define a lógica que deve ocorrer ao coletar a fruta.
+     *
+     * @param coletor O jogador que coletou a fruta.
+     */
+    public void aoColetar(Jogador coletor) {
+        // Lógica ao coletar fruta
+    }
 }
