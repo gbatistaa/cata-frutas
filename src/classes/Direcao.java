20a456a7b15ca44ca11fadb1feca8c94ec08b32b
Gabriel Batista <127104230+gbatistaa@users.noreply.github.com>
2024-10-09 23:10:49 -0300

    javadoc

--- src/classes/Direcao.java
+++ src/classes/Direcao.java
@@ -1,8 +1,14 @@
+/**
+ * O enum Direcao representa as direções possíveis no jogo.
+ */
 package classes;
 
+/**
+ * Enum representando as direções possíveis para o movimento.
+ */
 public enum Direcao {
-  CIMA,
-  BAIXO,
-  ESQUERDA,
-  DIREITA
+    CIMA,
+    BAIXO,
+    ESQUERDA,
+    DIREITA
 }
