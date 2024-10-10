baaaf8fe3c7f94797a64d67b604e5759ecba3a34
Gabriel Batista <127104230+gbatistaa@users.noreply.github.com>
2024-10-09 23:11:04 -0300

    javadoc

--- src/classes/Entidade.java
+++ src/classes/Entidade.java
@@ -1,50 +1,36 @@
+/**
+ * A classe Entidade é uma classe abstrata que representa uma entidade no jogo, como jogadores ou frutas.
+ */
 package classes;
 
 public abstract class Entidade {
-  private int x, y;
-  private boolean estatico = false;
+    private int x;
+    private int y;
 
-  public int getX() {
-    return x;
-  }
-
-  public void setX(int x) {
-    this.x = x;
-  }
-
-  public int getY() {
-    return y;
-  }
-
-  public void setY(int y) {
-    this.y = y;
-  }
-
-  public boolean isEstatico() {
-    return estatico;
-  }
-
-  public void setEstatico(boolean estatico) {
-    this.estatico = estatico;
-  }
-
-  public void movimentar(int novoX, int novoY) {
-    this.x = novoX;
-    this.y = novoY;
-  }
-
-  public void movimentar(Direcao dir) {
-    switch (dir) {
-      case CIMA:
-        break;
-      case BAIXO:
-        break;
-      case ESQUERDA:
-        break;
-      case DIREITA:
-        break;
-      default:
-        break;
+    public int getX() {
+        return x;
     }
-  }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * Move a entidade para uma nova posição.
+     *
+     * @param deltaX A mudança na coordenada x.
+     * @param deltaY A mudança na coordenada y.
+     */
+    public void mover(int deltaX, int deltaY) {
+        this.x += deltaX;
+        this.y += deltaY;
+    }
 }
